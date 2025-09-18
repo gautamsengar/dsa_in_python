4d21b2afe62a7602736bbd72f7874503dba9ece6
Gautam Singh Sengar <sengargautamsingh@gmail.com>
2025-09-18 22:59:04 +0530

    added factorial using my logic recursion

---
+++ factorial_recursion_myLogic.py
@@ -0,0 +1,10 @@
+#factorial using recursion
+number = int(input("Enter the number whose factorial you want : "))
+def fact(number):
+    if number==1 or number ==0   :
+        return 1
+    else:
+        return number * fact(number-1)
+    
+print("the factorial of given number is ",fact(number))
+

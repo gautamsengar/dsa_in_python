be2818e0297f661878a0b37470c02aeb21b5cf54
Gautam Singh Sengar <sengargautamsingh@gmail.com>
2025-09-22 09:27:13 +0530

    added fibonacci number dsa problem

---
+++ Find_the_Fibonacci_ Number.py
@@ -0,0 +1,11 @@
+class Solution:
+ def fib(self,num):
+  self.num= num
+  if self.num==0 or self.num==1:
+   return num
+  else:
+   return self.fib(num-1)+self.fib(num-2)
+answer=Solution()
+print(answer.fib(6))
+
+ 
